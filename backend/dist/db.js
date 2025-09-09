cfde68a6fb9c283d65df0ca3263b9c81d7da3704
Aniket0025 <aniketyadav25012005@gmail.com>
2025-09-09 20:02:01 +0530

    inital commit

---
+++ backend/dist/db.js
@@ -0,0 +1,8 @@
+import mongoose, { model, Schema } from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/second_brain");
+const userSchema = new Schema({
+    username: { type: String, unique: true },
+    password: { type: String }
+});
+export const UserModel = model("User", userSchema);
+//# sourceMappingURL=db.js.map
---
+++ backend/dist/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { UserModel } from "./db.js";
+const app = express();
+app.use(express.json());
+app.post("/api/v1/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    await UserModel.create({
+        username: username,
+        password: password
+    });
+    res.json({
+        message: "User Signed up"
+    });
+});
+app.post("/api/v1/signin", (req, res) => {
+    const username = req.body.username;
+});
+app.post("/api/v1/content", (req, res) => {
+});
+app.get("/api/v1/content", (req, res) => {
+});
+app.delete("/api/v1/content", (req, res) => {
+});
+app.post("/api/v1/brain/share", (req, res) => {
+});
+app.get("/api/v1/brain/:shareLink", (req, res) => {
+});
+app.listen(3000);
+//# sourceMappingURL=index.js.map
